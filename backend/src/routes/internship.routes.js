7cbd78e93e099ba79aca200a624cb2974e2230e6
ALok Pandey <114446066+alok-07-pandey@users.noreply.github.com>
2025-09-26 13:45:40 +0530

    Update internship.routes.js

--- backend/src/routes/internship.routes.js
+++ backend/src/routes/internship.routes.js
@@ -1,12 +1,24 @@
 // src/routes/internship.routes.js
-import { Router } from "express";
-import { findInternships } from "../controllers/internship.controller.js";
-import { protect } from "../middlewares/auth.middleware.js";
+import express from "express";
+import { submitInternship } from "../controllers/internship.controller.js";
 
-const router = Router();
-router.post("/find", protect, findInternships);
+const router = express.Router();
+
+router.post("/submit", submitInternship);
 
 export default router;
+
+
+
+// // src/routes/internship.routes.js
+// import { Router } from "express";
+// import { findInternships } from "../controllers/internship.controller.js";
+// import { protect } from "../middlewares/auth.middleware.js";
+
+// const router = Router();
+// router.post("/find", protect, findInternships);
+
+// export default router;
 
 
 
