89e5b2b7c684c305de82bcf91bfdab38e2f94bbd
ALok Pandey <114446066+alok-07-pandey@users.noreply.github.com>
2025-09-26 11:48:05 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -8,8 +8,7 @@ const userSchema = new Schema(
     username: { type: String, required: true, unique: true, lowercase: true, trim: true, index: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     fullName: { type: String, required: true, trim: true, index: true },
-    avatar: { type: String, required: true },
-    coverImage: { type: String },
+  
     password: { type: String, required: true },
     refreshToken: { type: String },
 }, { timestamps: true });
